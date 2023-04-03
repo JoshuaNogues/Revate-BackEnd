8e2c7bbebb1864c6b6184842dd0fe5b33b8a09cc
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2023-04-03 11:26:43 -0400

    set up basic backend

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var createError = require('http-errors');
+var createError = require("http-errors");
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var cors = require('cors')
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth')
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('trust proxy', 1);
+app.enable('trust proxy');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,23 +20,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URI]  // <== URL of our future React app
+    })
+  );
+
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+    next(createError(404));
+  });  
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
 
 module.exports = app;
